e94221fb816fc20e56e4e49803f9af24ccfad5f7
mayooraraj <mayooraminnu19@gmail.com>
2023-11-24 17:48:51 +0530

    second commit

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -9,12 +9,12 @@ export class StudentsComponent {
   student = [
     { id: 1, name: 'Mayoora', gender: 'female' },
     { id: 2, name: 'Amal', gender: 'male' },
-    { id: 1, name: 'Kavya', gender: 'female' },
-    { id: 2, name: 'Ajal', gender: 'male' },
-    { id: 1, name: 'Nandana', gender: 'female' },
-    { id: 2, name: 'Dileep', gender: 'male' },
-    { id: 1, name: 'Athira', gender: 'female' },
-    { id: 2, name: 'Nithin', gender: 'male' },
+    { id: 3, name: 'Kavya', gender: 'female' },
+    { id: 4, name: 'Ajal', gender: 'male' },
+    { id: 5, name: 'Nandana', gender: 'female' },
+    { id: 6, name: 'Dileep', gender: 'male' },
+    { id: 7, name: 'Athira', gender: 'female' },
+    { id: 8, name: 'Nithin', gender: 'male' },
    
   ];
 
